2c205a91d32d5bd3450cb81b17aff0188c15241c
kimkyusig <big35@naver.com>
2024-01-08 16:44:11 +0900

    20240108

--- 04_control/src/pkg03_control_practice/MainClass.java
+++ 04_control/src/pkg03_control_practice/MainClass.java
@@ -5,15 +5,23 @@ public class MainClass {
   public static void method1() {
     
     // 월에 따른 계절 출력하기
-    int month = 7;
+    int month = 1;
+    String season;
     
-    if(month <= 2) System.out.println("겨울");
-    else if (month <= 5) System.out.println("봄");
-    else if (month <= 8) System.out.println("여름");
-    else if (month <= 11)System.out.println("가을");
+    int mod = month % 12;
     
+    if(mod <= 2) {
+      season = "겨울";
+    } else if(mod <= 5) {
+      season = "봄";
+    } else if(mod <= 8) {
+      season = "여름";
+    } else {
+      season = "가을";
+    }
+    
+    System.out.println(season);
   }
-  
   public static void method2() {
     
     // 점수와 학년에 따른 합격 여부
@@ -21,35 +29,93 @@ public class MainClass {
     // 4~6학년 : 70점 이상이면 합격, 아니면 불합격
     
     int score = 68;  // 점수
-    int grade = 6;   // 학년
+    int grade = 1;   // 학년
     
-    if(grade <= 3) {
+      if(grade <= 3) {
       if(score >= 60) System.out.println("합격");
       else System.out.println("불합격");
     }
     else if(grade <= 6) {
       if(score >= 70) System.out.println("합격");
       else System.out.println("불합격");
+    } 
+    int pass = 0;
+    if(grade >= 1 && grade <= 3) {
+      pass = 60;
+    } else if(grade >= 4 && grade <= 6) {
+      pass= 70;
     }
+    //합격 불합격
+    String result= null;
+    if(score >= pass) {
+      result = "합격";
+    } else {
+      result = "불합격";
+    }
+    System.out.println(result);
   }
   
   public static void method3() {
     
     // 4일은 목요일, n일 후는 무슨 요일?
-    int dayy = day +nDay;
-    int day = 4;      // 4일은 목요일
-    int nDay = 10;    // nDay일 후
-    String weekName;  // 요일 (월~일)
-    
-    if(dayy == 14) {
-      String weekName = "일요일";?????
-    System.out.println(weekName);
+        
+        int day = 4;      // 4일은 목요일
+        int nDay = 10;    // nDay일 후
+        String weekName;  // 요일 (월~일)
+        
+        // day의 nDay일 후
+        day += nDay; 
+        // day를 7로 나눈 나머지
+        int mod = day % 7;
+        if(mod == 0)
+          weekName = "일";
+        else if(mod == 1)
+          weekName = "월";
+        else if(mod == 2)
+          weekName = "화";
+        else if(mod == 3)
+          weekName = "수";
+        else if(mod == 4)
+          weekName = "목";
+        else if(mod == 5)
+          weekName = "금";
+        else
+          weekName = "토";
+        
+        System.out.println(weekName);
     }
     
-  }
+   public static void method4() {
+     
+     // 메뉴에 따른 가격
+     // 아메리카노 : 1000
+     // 카페라떼   : 2000
+     // 밀크티     : 3000
+     // 기타       : 5000
+     
+     String order = "녹차";
+     int price = 0;
+     
+     switch(order) {
+     case "아메리카노":
+       price = 1000;
+       break;
+     case "카페라떼":
+       price = 2000;
+       break;
+     case "밀크티":
+       price = 3000;
+       break;
+     default:
+       price = 5000;  
+     }
+     
+     System.out.println(price);
+     
+   }
   
   public static void main(String[] args) {
- method3();
+ method2();
   }
 
-}
+} 
---
+++ 04_control/src/pkg04_loop_practice/MainClass.java
@@ -0,0 +1,246 @@
+package pkg04_loop_practice;
+
+public class MainClass {
+
+  public static void method1() {
+    // 100 ~ 1 출력하기
+    int n =100;
+    
+    while( n >= 1 ) {
+      System.out.println(n--);  //출력후 감소
+      
+    }
+    
+  }
+  
+  public static void method2() {
+    
+ // 1 ~ 4 사이 모든 정수의 평균 (합계/개수)
+    int begin = 1;
+    int end = 4;
+    
+    int n = begin;
+    int total = 0;
+    while(n <= end) {
+      total += n;
+      n++;
+    }
+    System.out.println(total);
+    double average = total / (end - begin + 1.0);
+    System.out.println(average); 
+    
+      
+     
+    }
+    
+  public static void method3() {
+    
+    // 3단 출력하기
+    
+     
+  
+    int dan =3;
+    for(int n =1; n<=9; n++) {
+      System.out.println(dan + "x"+ n + "=" + dan * n);   // 플러스로 연결을 위해서는 반드시"" 사용
+      
+     
+    }
+      
+   
+    
+    
+  }
+  
+  public static void method4() {
+    
+    // 70원씩 모금하기
+    // 목표는 100000원 이상
+    
+    // 1회 모금액 70원
+    // 2회 모금액 140원
+    // 3회 모금액 210원
+    // ..
+    // 1429회 모금액100030원
+    
+    int money = 70;              //1회 모금액
+    int total = 0;             // 전체 모금액
+    final int GOAL = 100_000;  // 목표
+    int nth = 0;               //회차  
+    
+    while(total < GOAL) {
+      total += money;
+      nth++;
+      System.out.println(nth + "회 모금액" + total + "원");
+    }
+    
+      
+    
+  }
+
+  public static void method5(){
+    // 2x1=2
+    // 2x2=4
+    // ....
+    // 5x5 = 25
+    outer:  //라벨
+    for(int dan =2; dan <=9; dan++) { 
+      for(int n =1; n <=9; n++) {
+        System.out.println(dan + "x" + n + "=" + dan * n);
+        if(dan ==5 && n== 5) {
+          break outer;
+        }
+      }
+    }
+    
+      
+    
+    
+  }
+  
+  public static void method6() {
+    
+    // 2x1=2   3x1=3 ...   9x1=9
+    // 2x2=4   3x2=6 ...   9x2=18
+    
+    for(int n=1; n<=9; n++) {  
+      for(int dan = 2; dan <=9; dan++) {
+        System.out.print(dan + "x" + n + "=" + dan * n + "\t");
+      }
+      System.out.println();  // 줄 바꿈
+    }
+            
+  }
+  
+  public static void method7() {
+
+    /*
+     * 삼각별 출력하기-1
+     *        star=12345 
+     * row=1       * 
+     * row=2       ** 
+     * row=3       *** 
+     * row=4       **** 
+     * row=5       *****
+     * 
+     * row=1, star=1~1 
+     * row=2, star=1~2 
+     * row=3, star=1~3 
+     * row=4, star=1~4 
+     * row=5, star=1~5
+     * 
+     * row =1~5, star = 1~row
+     */
+    
+    for(int row=1; row <=5; row++) {
+      for(int star=1; star<=row; star++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+
+    
+    
+  }
+  
+  public static void method8() {
+    
+    /*
+     * 삼각별 출력하기-2
+     *        star=12345
+     * row=1       *****
+     * row=2       ****
+     * row=3       ***
+     * row=4       **
+     * row=5       *
+     * 
+     * row=1, star=1~5
+     * row=2, star=1~4
+     * row=3, star=1~3
+     * row=4, star=1~2
+     * row=5, star=1~1
+     * 
+     * row=1~5 star=1~6-row
+     */
+    for(int row=1; row <=5; row++) {
+      for(int star=1; star <=6 -row; star++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    
+    
+  }
+  
+  public static void method9() {
+    
+    /*
+     * 삼각별 출력하기-3
+     * space/star=123456789
+     * row=1          *
+     * row=2         ***
+     * row=3        *****
+     * row=4       *******
+     * row=5      *********
+     * 
+     * row=1, space=1~4, star=5~5
+     * row=2, space=1~3, star=4~6
+     * row=3, space=1~2, star=3~7
+     * row=4, space=1~1, star=2~8
+     * row=5, space=1~0, star=1~9
+     * 
+     * row=1~5 space=1~5-row, star=6-row~row+4
+     */
+    for(int row=1; row<=5;row++) {
+      for(int space=1; space <= 5-row; space++) {
+        System.out.print(" ");
+      }
+      for(int star =6 -row; star <= row + 4; star++) {
+        System.out.print("*");
+      } 
+      System.out.println();
+    }
+    
+    
+
+  }
+  
+  public static void method10() {
+    
+    /*
+     * 삼각별 출력하기-4
+     * space/star=123456789
+     * row=1      *********
+     * row=2       *******
+     * row=3        *****
+     * row=4         ***
+     * row=5          *
+     * 
+     * row=1, space=1~0, star=1~9
+     * row=2, space=1~1, star=2~8
+     * row=3, space=1~2, star=3~7
+     * row=4, space=1~3, star=4~6
+     * row=5, space=1~4, star=5~5
+     * 
+     * row=1~5, space=1~row-1, star=row,10-row~
+     */
+    for(int row =1; row<=5; row++) {
+      for(int space=1; space<=row-1; row++) {
+        System.out.print(" ");
+      }
+      for(int star=row; row<= 10-row; row++) {
+        System.out.println("*");
+      }
+      System.out.println();
+    }
+    
+    
+  }
+  
+  
+  public static void main(String[] args) {
+
+    method3();
+    
+  }
+
+}
